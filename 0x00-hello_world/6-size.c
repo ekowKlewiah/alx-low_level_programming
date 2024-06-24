d6acab16e9581ee9f6dd2551507b84c8ee041529
ekowKlewiah <klewsoke@gmail.com>
2024-06-25 00:06:31 +0100

    compile

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,9 @@
 
 int main(void)
 {
-	printf("size of char: %zu byte(s)\n", size off(char))'
+	printf("size of char: %zu byte(s)\n", sizeof(char))'
 	printf("Size of short: %zu byte(s)\n", sizeof(short));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
-       	printf("Size of long: %zu byte(s)\n, sizeof(long));
-	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-	printf("size of the float: %zu byte(s)\n", sizeof(float));
-	printf("Size of the double: %zu bytes(s)\n", sizeof(double));
-        printf("Size of long double: %zu bytes(s)\n", sizeof(long double));
 
 return (0);
 
