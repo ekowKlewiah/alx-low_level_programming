d52080fd6bc8e2c59f663ccaec1c0ab4dae10030
ekowKlewiah <klewsoke@gmail.com>
2024-06-24 19:03:48 +0100

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	printf("size of char: %zu byte(s)\n", size off(char))'
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+       	printf("Size of long: %zu byte(s)\n, sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("size of the float: %zu byte(s)\n", sizeof(float));
+	printf("Size of the double: %zu bytes(s)\n", sizeof(double));
+        printf("Size of long double: %zu bytes(s)\n", sizeof(long double));
+
+return (0);
+
+}
