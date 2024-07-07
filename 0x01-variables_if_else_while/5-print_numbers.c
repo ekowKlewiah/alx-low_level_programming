477ec5bd8f2e55e6f0fee8efcf3f8fbd637286a2
ekowKlewiah <klewsoke@gmail.com>
2024-07-07 23:28:23 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * body of code
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+
+{
+	int r;
+
+	for (r = 0; r <= 9; r++)
+		printf("%i", r);
+	printf("\n");
+
+	return (0);
+}
