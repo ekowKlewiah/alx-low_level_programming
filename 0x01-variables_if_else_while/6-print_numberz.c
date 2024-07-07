e5c48756d76a0aec1465b56889c3aafc1bf9f2ef
ekowKlewiah <klewsoke@gmail.com>
2024-07-07 23:38:44 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * code body
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+
+{
+	int r = 0;
+
+	for (; r < 10; r++)
+		putchar('0' + r);
+	putchar('\n');
+
+	return (0);
+}
