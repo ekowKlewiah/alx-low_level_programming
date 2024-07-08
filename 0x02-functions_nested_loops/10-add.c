8a94d00a8bedb6a84746f8e700e9b3a62ba2ec39
ekowKlewiah <klewsoke@gmail.com>
2024-07-08 14:24:38 +0100

    adding 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - takes 2 integers as params
+ * @n1: first integer param
+ * @n2: second integer param
+ *
+ * Return: result of n1 + n2
+ */
+
+int add(short n1, short n2)
+
+{
+	return (n1 + n2);
+}
