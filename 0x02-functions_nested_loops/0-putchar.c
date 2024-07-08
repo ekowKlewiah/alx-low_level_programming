8fd9e5a012fb7a7af06619bd71ecdb628bd9736f
ekowKlewiah <klewsoke@gmail.com>
2024-07-08 13:21:05 +0100

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * code body
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+
+{
+
+const char *Text = "_putchar";
+unsigned short i;
+
+for (i = 0; i <= 7; i++)
+{
+	_putchar(Text[i]);
+}
+_putchar('\n');
+
+return (0);
+
+}
