4797ec096b7ab3dd0fb2ef8bf7e2a3e8a31731ee
ekowKlewiah <klewsoke@gmail.com>
2024-07-08 13:35:20 +0100

    main file created

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _print_chars();
+void print_alphabet();
+void print_alphabet_x10();
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int Num);
+int print_last_digit(int Num);
+void _triplets(unsigned short n1, unsigned short n2, char c);
+void jack_bauer(void);
+void times_table(void);
+int add(short n1, short n2);
+void print_to_98(int n);
+void print_times_table(int n);
+int sum_numbers(void);
+void print_first_92_fib_numbers(void);
+void print_remaining_fib_numbers(void);
+
+
+#endif
