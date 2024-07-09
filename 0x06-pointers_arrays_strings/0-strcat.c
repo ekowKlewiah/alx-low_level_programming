9f1c91c378692cdb574148bae47bf14101871ed1
ekowKlewiah <klewsoke@gmail.com>
2024-07-09 04:55:04 +0100

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,40 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
+#include <string.h>
 
-#ifndef MAIN_H
-#define MAIN_H
 /**
- *main - check the code
+ * _strcat - function to append strings
+ * @dest: input
+ * @src: input
  *
- *Return: Always 0 (Success)
+ * Return: char
  */
 
-
-
-#endif
-
-char *con(char *a, char *b)
+char *_strcat(char *dest, char *src)
 {
-   int i;
-   for (i=0; a[i] != '\0'; i++)
-	   ;
-
-   for (int j=0; b[j] !='\0'; j++)
-      {
-        a[i] = b[j];
-	i++;
-      }
-   return (a);
-}
-
-int main(void) {
-	  char s1[98] = "Hello ";
-	  char s2[] = "World!\n";
-	  char *ptr;
-	 
-	  ptr = con(s1, s2);
-	  
-	  printf("%s",ptr);
-	   
-	   return (0);
+	return (strcat(dest, src));
 }
